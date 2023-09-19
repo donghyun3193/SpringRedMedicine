443b6b58d215964518fc6ebd1ab80f830cced47e
NohEuijin <141835418+NohEuijin@users.noreply.github.com>
2023-09-19 19:39:07 +0900

    헤더 푸터 적용 및 로그인 페이지 이동

--- src/main/java/com/example/redmedicine/controller/UserController.java
+++ src/main/java/com/example/redmedicine/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.redmedicine.controller;
 import com.example.redmedicine.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +13,9 @@ public class UserController {
 
     private final UserService userService;
 
+    //로그인페이지
+    @GetMapping("login")
+    public String showLoginPage(){
+        return "user/login";
+    }
 }
