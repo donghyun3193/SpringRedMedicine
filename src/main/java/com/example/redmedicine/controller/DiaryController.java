2105c200d39e1d0759e8fb4833fb6e7346c9a0fc
JiyoonLee724 <jiyoon0273@naver.com>
2023-09-23 17:57:01 +0900

    2023/09/23 -지윤 -공지사항 페이지 글 작성 시 db에 데이터 들어가도록 처리 -공지사항 페이지 글 작성 후 방금 작성한 글 상세페이지로 이동, 글 정보 불러오기

--- src/main/java/com/example/redmedicine/controller/DiaryController.java
+++ src/main/java/com/example/redmedicine/controller/DiaryController.java
@@ -1,16 +1,16 @@
 package com.example.redmedicine.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board/*")
-public class BoardController {
-
-    //공지사항 페이지
-    @GetMapping("/notice")
-    public String showNoticePage(){return "board/notice";}
+@RequiredArgsConstructor
+@Slf4j
+public class DiaryController {
 
     //일기쓰기 페이지
     @GetMapping("/diary")
