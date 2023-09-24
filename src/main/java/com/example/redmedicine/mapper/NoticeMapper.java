41f084a56d809c5db57fa8f4cbc12ef77dcad95e
JiyoonLee724 <jiyoon0273@naver.com>
2023-09-24 22:36:39 +0900

    *공지사항 리스트 불러오기 -한 페이지 당 글 10개씩 불러오기 *공지사항 리스트 페이징처리 -페이지 번호 클릭 시 해당 페이지로 이동, 다음/이전 버튼 클릭 시 다음/이전 세트로 이동 *공지사항 글 제목 클릭 시 해당 글 상세 페이지로 이동 처리 *수정 ,삭제 -수정 버튼 클릭 시 수정페이지로 이동/ 해당 글의 정보 화면에 뿌림(제목,내용) -삭제 버튼 클릭 시 "글을 삭제하시겠습니까?" 확인창 띄우고 확인버튼 클릭 시 해당 글 목록과 DB에서 삭제처리

--- src/main/java/com/example/redmedicine/mapper/NoticeMapper.java
+++ src/main/java/com/example/redmedicine/mapper/NoticeMapper.java
@@ -1,14 +1,29 @@
 package com.example.redmedicine.mapper;
 
 import com.example.redmedicine.domain.dto.NoticeDto;
+import com.example.redmedicine.domain.vo.Criteria;
 import com.example.redmedicine.domain.vo.NoticeVo;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface NoticeMapper {
-    //    추가
+    //추가
     public void insert(NoticeDto noticeDto);
 
-    //    조회
+    //조회
     public NoticeVo select(Long noticeNumber);
+
+    //전체 조회
+    public List<NoticeVo> selectAll(Criteria criteria);
+
+    //전체 게시물 수 조회
+    public int selectTotal();
+
+    //    삭제
+    public void delete(Long noticeNumber);
+
+    //    수정
+    public void update(NoticeDto noticeDto);
 }
