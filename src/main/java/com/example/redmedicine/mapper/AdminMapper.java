07df308581634936d9100dc26350a9b6a88bfc73
umurf <yoodh1536@gmail.com>
2023-09-25 20:47:39 +0900

    유대훈 회원정보관리 페이징 수정 완료, 회원 삭제 기능 완료, 검색기능 작업중

--- src/main/java/com/example/redmedicine/mapper/AdminMapper.java
+++ src/main/java/com/example/redmedicine/mapper/AdminMapper.java
@@ -2,7 +2,10 @@ package com.example.redmedicine.mapper;
 
 import com.example.redmedicine.domain.dto.UserDto;
 import com.example.redmedicine.domain.vo.Criteria;
+import com.example.redmedicine.domain.vo.PageVo;
+import com.example.redmedicine.domain.vo.SearchVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -13,10 +16,13 @@ public interface AdminMapper {
     public UserDto select(Long userNumber);
 
     //전체 회원 조회
-    public List<UserDto> selectAll(Criteria criteria);
+    public List<UserDto> selectAll(@Param("criteria") Criteria criteria, @Param("searchVo") SearchVo searchVo);
 
 //    전체 회원 수 조회
     public int selectTotal();
+
+//    회원 삭제
+    public void delete(Long userNumber);
 
 
 }
