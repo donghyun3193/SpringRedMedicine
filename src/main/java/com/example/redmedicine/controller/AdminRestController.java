71a42bf30d343fd7334e9cc50576e03defefc3ee
umurf <yoodh1536@gmail.com>
2023-09-26 17:47:32 +0900

    유대훈 회원정보 관리 페이지 완성..!

--- src/main/java/com/example/redmedicine/controller/AdminRestController.java
+++ src/main/java/com/example/redmedicine/controller/AdminRestController.java
@@ -2,12 +2,15 @@ package com.example.redmedicine.controller;
 
 import com.example.redmedicine.domain.dto.UserDto;
 import com.example.redmedicine.domain.vo.Criteria;
+import com.example.redmedicine.domain.vo.PageVo;
 import com.example.redmedicine.domain.vo.SearchVo;
 import com.example.redmedicine.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/admin")
@@ -15,9 +18,18 @@ import java.util.List;
 public class AdminRestController {
     private final AdminService adminService;
 
-    @GetMapping(value = {"/{page}"})
-    public List<UserDto> searchResult(Criteria criteria, SearchVo searchVo){
-        return adminService.findAll(criteria, searchVo);
+    @GetMapping(value = {"/memberShipMm/{page}"})
+    public Map<String, Object> searchResult(@PathVariable("page") int page, SearchVo searchVo){
+
+        System.out.println(searchVo);
+
+        Criteria criteria = new Criteria();
+        criteria.setPage(page);
+        PageVo pageVo = new PageVo(adminService.searchTotal(searchVo), criteria);
+        Map<String, Object> map = new HashMap<>();
+        map.put("pageVo",pageVo);
+        map.put("list",adminService.findAll(criteria, searchVo));
+        return map;
     }
 
 }
