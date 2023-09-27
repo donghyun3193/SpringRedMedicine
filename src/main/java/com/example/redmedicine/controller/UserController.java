8bd0d17596ea0626e875c9e73fc11f21e3b5906d
JiyoonLee724 <jiyoon0273@naver.com>
2023-09-27 17:32:40 +0900

    아이디 찾기-지윤 이름, 휴대폰 번호 입력 후 아이디찾기 버튼 클릭 시 -일치하는 회원정보가 있을 경우 : div 나타나고 div 안에 해당 회원의 id 표시 -일치하는 회원정보가 없을 경우 : div 나타나고 div 안에 "일치하는 회원 정보가 없습니다." 표시

--- src/main/java/com/example/redmedicine/controller/UserController.java
+++ src/main/java/com/example/redmedicine/controller/UserController.java
@@ -6,9 +6,7 @@ import com.example.redmedicine.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
