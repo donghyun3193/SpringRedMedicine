3442238e22a18a0b099e580ec11db0d44ebc2415
umurf <yoodh1536@gmail.com>
2023-10-05 13:28:43 +0900

    no message

--- src/main/java/com/example/redmedicine/mapper/BookingMapper.java
+++ src/main/java/com/example/redmedicine/mapper/BookingMapper.java
@@ -1,7 +1,10 @@
 package com.example.redmedicine.mapper;
 
+import com.example.redmedicine.domain.dto.BookDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface BookingMapper {
+    //
+    public BookDto insert();
 }
