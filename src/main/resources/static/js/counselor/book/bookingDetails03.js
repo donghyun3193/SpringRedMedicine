daff879c6e594c1a24ce76d65702d0ca1ebf2b2e
NohEuijin <141835418+NohEuijin@users.noreply.github.com>
2023-10-07 19:30:16 +0900

    커밋1

--- src/main/resources/static/js/counselor/book/bookingDetails03.js
+++ src/main/resources/static/js/counselor/book/bookingDetails03.js
@@ -51,14 +51,8 @@ submitButton.addEventListener("click", function () {
 
     alert("예약이 완료되었습니다.");
 
-
-<<<<<<< HEAD
     sessionStorage.clear();
-
-    window.location.href = "/src/main/resources/templates/main/index.html";
-=======
->>>>>>> jiyoon1007
-
+    
 });
 
 
